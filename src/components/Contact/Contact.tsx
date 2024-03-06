3c0dc41a93bb1307d65ef021a0c71ad6aa768cf2
Game Limsila <limsila.limsila@yahoo.com>
2024-03-06 13:12:10 +0800

    updated info

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -57,6 +57,38 @@ export default function Contact() {
           </Link>
         </div> */}
       </div>
+      <div className="flex items-center justify-center">
+
+        <ol className="relative border-s border-accentColor dark:border-gray-700 ">
+          <li className="mb-10 ms-4">
+            <div className="absolute w-3 h-3 bg-accentColor rounded-full mt-2.5 -start-1.5 border dark:border-gray-900 dark:bg-gray-700"></div>
+            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2024</time>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Nanyang Technological University</h3>
+            <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Exchange at School of Electrical and Electronic Engineering</p>
+            {/* <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Learn more <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+            </svg></a> */}
+          </li>
+          <li className="mb-10 ms-4">
+            <div className="absolute w-3 h-3 bg-accentColor rounded-full mt-2.5 -start-1.5 border dark:border-gray-900 dark:bg-gray-700"></div>
+            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2021 - 2025</time>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Chulalongkorn University, Bangkok, Thailand</h3>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">Bachelor in Robotics and Artifical Intelligence Engineering</p>
+          </li>
+          <li className="ms-4">
+            <div className="absolute w-3 h-3 bg-accentColor rounded-full mt-2.5 -start-1.5 border dark:border-gray-900 dark:bg-gray-700"></div>
+            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">April 2022</time>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Lanna International School, Chaing Mai, Thailand</h3>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">Cambrige International Examination A-Levels </p>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">  - Physics</p>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">  - Computer Science</p>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">  - Maths (Mechanics and Statistic)</p>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">  - Chemistry</p>
+          </li>
+        </ol>
+
+
+      </div>
 
 
       {/* <div className="absolute left-1/2 max-w-6xl -translate-x-1/2">
--- src/components/Experiences/Experience.tsx
+++ src/components/Experiences/Experience.tsx
@@ -7,7 +7,7 @@ import jsonData from "../../../public/experience.json";
 
 export function Publications() {
   return (
-    <section className="min-h-screen  sm:mx-16 md:mx-24 xl:mx-40 ">
+    <section className="min-h-screen  pb-10 sm:mx-16 md:mx-24 xl:mx-40 ">
       {/* <section className="min-h-screen  sm:mx-16 md:mx-24 xl:mx-40 "> */}
       <Typography type="h1" className="">
         {"Robocup"}
@@ -18,10 +18,15 @@ export function Publications() {
       <Typography type="p" className="font-bold">
         {"Job as Vice-President and Natural Language Processing(NLP) Lead"}
       </Typography>
-      <Typography type="p" className="">
+      <Typography type="p" className="pb-1">
         {
-          "Led the organization of our team's participation in the Robocup@Home competition in Bordeaux, France. focusing on service robots for household assistance. Coordinating travel for 10 members to Bordeaux, overseeing our qualification for Robocup2023 through preparing a Team Description Paper, managing video production, and maintaining the official EIC website. "
-        }
+          "•	Spearheaded the creation of \"Walkie 3,\" a bespoke robot for Robocup@Home, overseeing team qualifications, crafting the Team Description Paper, managing video production, and arranging logistics for Bordeaux and Bangkok competitions. Focused on the software aspects, optimizing YOLOv8 with TensorRT for enhanced computer vision efficiency."
+        }</Typography>
+      <Typography type="p" className="pb-1">
+        {"•	Led the development of \"Walkie 3's\" brain with Python/ROS2 Humble, writing the majority of the SMACH state logic and implementing an on-the-fly state generator, streamlining complex behavior execution."}
+      </Typography>
+      <Typography type="p" className="pb-1">
+        {"•	Developed \"Walkie 3's\" interactive face, designing a Flutter-based UI for displaying sponsors and providing live transcription, enhancing audience engagement at Robocup@Home events."}
       </Typography>
       <Typography type="p" className="pb-10">
         {
@@ -51,6 +56,27 @@ export function Publications() {
       <Typography type="p" className="">
         {"An incredible experience! We finsihed second amognst other finalist"}
       </Typography>
+      {/* Grey bar divider */}
+      <hr className="h-px my-10 border-0 bg-slate-400"></hr>
+
+      <Typography type="h1" className="">
+        {"Web Dev"}
+      </Typography>
+      <Typography type="h2" className="">
+        {"MELLA E-commerence food platform previously sponsored by AISEC"}
+      </Typography>
+      <Typography type="p" className="font-bold">
+        {"Full Stack Developer and Backend Lead"}
+      </Typography>
+      <Typography type="p" className="pb-10">
+        {"•	Spearheaded the development and optimization of server-side applications, enhancing systemscalability and performance."}
+      </Typography>
+      <Typography type="p" className="pb-10">
+        {"•	Led a team in integrating APIs, databases, and storage systems to ensure seamless data flow and reliability"}
+      </Typography>
+      <Typography type="p" className="pb-10">
+        {"•	Developed responsive user interfaces using modern frameworks (NextJS), ensuring cross-platform compatibility"}
+      </Typography>
 
       {/* Grey bar divider */}
       <hr className="h-px my-10 border-0 bg-slate-400"></hr>
@@ -67,6 +93,8 @@ export function Publications() {
       <Typography type="p" className="">
         {"A robot design competition with contestants from all around the world."}
       </Typography>
+
+
     </section>
   );
 }
